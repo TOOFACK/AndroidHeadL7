b87c85b9056fe98fdcaa2c0cd8d832737bbc929e
pavel <paulrussianvodka@gmail.com>
2019-01-09 18:47:17 +0300

    Add code to DrinkCategoryActivity

--- app/src/main/java/com/example/paulr/androidheadl7/DrinkCategoryActivity.java
+++ app/src/main/java/com/example/paulr/androidheadl7/DrinkCategoryActivity.java
@@ -2,6 +2,8 @@ package com.example.paulr.androidheadl7;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class DrinkCategoryActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class DrinkCategoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_category);
+
+        ListView listDrinks = findViewById(R.id.list_drinks);
+
+        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<Drink>(this, android.R.layout.simple_list_item_1, Drink.drinks);
+        listDrinks.setAdapter(listAdapter);
     }
 }
